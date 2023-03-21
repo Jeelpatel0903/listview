c0048a48a658c1063442327aebcc61e436905b13
Jeelpatel0903 <71834522+Jeelpatel0903@users.noreply.github.com>
2023-03-21 21:16:28 +0530

    commit done

--- app/src/main/java/com/ajdev/viewsapp/MainActivity2.java
+++ app/src/main/java/com/ajdev/viewsapp/MainActivity2.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.ListView;
 import android.widget.Spinner;
 
@@ -20,10 +21,14 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         Spinner spn;
+        AutoCompleteTextView autocom;
 
         spn = findViewById(R.id.spnv);
+        autocom = findViewById(R.id.autocom);
 
         ArrayList<String> arrlist = new ArrayList<>();
+        ArrayList<String> arrcity = new ArrayList<>();
+
 
         arrlist.add("jeel");
         arrlist.add("dev");
@@ -33,11 +38,29 @@ public class MainActivity2 extends AppCompatActivity {
         arrlist.add("dhrashti");
 
 
-        Intent intext = new Intent(MainActivity2.this,MainActivity3.class);
-
-
-        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1,arrlist);
+        ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item,arrlist);
         spn.setAdapter(adapter);
+
+        arrcity.add("ahmedabad");
+        arrcity.add("surat");
+        arrcity.add("baroda");
+        arrcity.add("rajkot");
+        arrcity.add("jamnagar");
+        arrcity.add("panchmahal");
+        arrcity.add("surendrnagar");
+        arrcity.add("kathch");
+        arrcity.add("mandvi");
+        arrcity.add("gujrat");
+
+
+
+        //Intent intext = new Intent(MainActivity2.this,MainActivity3.class);
+
+
+
+        ArrayAdapter auto = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1,arrlist);
+        autocom.setAdapter(auto);
+        autocom.setThreshold(1);
 
 
     }
